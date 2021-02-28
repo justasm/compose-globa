604ca312806b2d7b6c3f7d72ca17fcc817a8a7ee
Justas Medeisis <justas@trafi.com>
2021-02-28 17:59:21 +0200

    Import LayoutDirection

--- app/src/main/java/com/example/androiddevchallenge/ui/PetListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PetListScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.pets
 import com.example.androiddevchallenge.model.Pet
