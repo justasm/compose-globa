8779d2b36834edd6d73e76e2c42de699582fb911
Justas Medeisis <justas@trafi.com>
2021-03-03 00:20:44 +0200

    Revamp the detail screen.

--- app/src/main/java/com/example/androiddevchallenge/ui/PetDetailScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PetDetailScreen.kt
@@ -16,16 +16,22 @@
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -33,7 +39,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -41,12 +49,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.pets
 import com.example.androiddevchallenge.model.Pet
+import com.example.androiddevchallenge.model.formatAgeYearsLabel
+import com.example.androiddevchallenge.model.formatAgeYearsRange
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
+import dev.chrisbanes.accompanist.insets.navigationBarsPadding
+import dev.chrisbanes.accompanist.insets.statusBarsPadding
+import java.util.Locale
 
 @Composable
 fun PetDetailScreen(pet: Pet, onBackClick: () -> Unit) {
     Surface(
-        color = MaterialTheme.colors.background,
+        color = MaterialTheme.colors.surface,
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
@@ -61,25 +75,72 @@ fun PetDetailScreen(pet: Pet, onBackClick: () -> Unit) {
                         .fillMaxWidth()
                         .aspectRatio(3 / 2f)
                 )
-                IconButton(onClick = onBackClick) {
+                IconButton(
+                    onClick = onBackClick,
+                    modifier = Modifier.statusBarsPadding()
+                ) {
                     Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                 }
             }
-            Column(Modifier.padding(horizontal = 32.dp, vertical = 32.dp)) {
+            Column(
+                Modifier
+                    .padding(horizontal = 32.dp, vertical = 32.dp)
+                    .navigationBarsPadding()
+            ) {
                 Text(
                     text = pet.name,
                     style = MaterialTheme.typography.h4,
                 )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Place,
+                        contentDescription = null,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    Text(
+                        text = "Vilnius, Lithuania",
+                        style = MaterialTheme.typography.caption,
+                    )
+                }
                 Spacer(Modifier.height(16.dp))
-                Text(
-                    text = "${pet.type} · ${pet.ageYearsFormatted} · ${pet.gender}",
-                    style = MaterialTheme.typography.body1,
-                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    PetDetailCard(
+                        value = pet.gender.toString(),
+                        description = "Sex",
+                        modifier = Modifier.weight(1f)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    PetDetailCard(
+                        value = pet.type.toString(),
+                        description = "Animal",
+                        modifier = Modifier.weight(1f)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    PetDetailCard(
+                        value = pet.formatAgeYearsRange(),
+                        description = pet.formatAgeYearsLabel().capitalize(Locale.US),
+                        modifier = Modifier.weight(1f)
+                    )
+                }
                 Spacer(Modifier.height(16.dp))
                 Text(
                     text = pet.description,
                     style = MaterialTheme.typography.body1,
                 )
+                Spacer(Modifier.height(16.dp))
+                Button(
+                    onClick = { },
+                    contentPadding = PaddingValues(16.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Get in touch")
+                }
             }
         }
     }
@@ -88,15 +149,19 @@ fun PetDetailScreen(pet: Pet, onBackClick: () -> Unit) {
 @Preview("Light Theme")
 @Composable
 private fun PetDetailScreenLightPreview() {
-    MyTheme {
-        PetDetailScreen(pets[0], onBackClick = {})
+    ProvideWindowInsets {
+        MyTheme {
+            PetDetailScreen(pets[0], onBackClick = {})
+        }
     }
 }
 
 @Preview("Dark Theme")
 @Composable
 private fun PetDetailScreenDarkPreview() {
-    MyTheme(darkTheme = true) {
-        PetDetailScreen(pets[0], onBackClick = {})
+    ProvideWindowInsets {
+        MyTheme(darkTheme = true) {
+            PetDetailScreen(pets[0], onBackClick = {})
+        }
     }
 }
